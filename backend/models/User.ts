383651d27e7293d217675243efeca69567defd80
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-02-16 20:41:42 +0100

    Add User model with Mongoose schema and IUser interface

---
+++ backend/models/User.ts
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from "mongoose";
+import IUser from "../interfaces/IUser";
+
+const UserSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model<IUser>("User", UserSchema);
+export default User;
