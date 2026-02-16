45025c1aa24656ed10e7a4e5e70e9159e314e9a2
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-02-16 21:21:35 +0100

    Add error handling middleware to manage server errors

--- backend/server.ts
+++ backend/server.ts
@@ -2,10 +2,14 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import errorHandler from "./middlewares/errorMiddleware";
 
 dotenv.config();
 
 const app = express();
+
+app.use(errorHandler);
+
 app.use(cors());
 app.use(express.json());
 
