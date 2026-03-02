6eee0cb24644809563439d37119107c5dc0ba743
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-03-02 19:20:57 +0100

    feat: enhance AuthForm component with conditional rendering for sign up and sign in

--- frontend/src/components/auth/AuthForm.tsx
+++ frontend/src/components/auth/AuthForm.tsx
@@ -1,12 +1,25 @@
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const AuthForm: React.FC = () => {
+  const pathName = usePathname();
   return (
-    <form>
-      <input type="text" />
-      <input type="email" name="email" />
-      <input type="password" name="password" />
-      <input type="password" name="confirm password" />
+    <form className="flex flex-col">
+      {pathName === "/signUp" && (
+        <input type="text" name="name" placeholder="Name" />
+      )}
+      <input type="email" name="email" placeholder="Email" />
+      <input type="password" name="password" placeholder="Password" />
+      {pathName === "/signUp" && (
+        <input
+          type="password"
+          name="confirm password"
+          placeholder="Confirm Password"
+        />
+      )}
+      <button type="submit" className="cursor-pointer">
+        {pathName === "/signUp" ? "Sign Up" : "Sign In"}
+      </button>
     </form>
   );
 };
