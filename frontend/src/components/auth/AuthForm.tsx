eb5c6a35fc2f6de88250d646a495b0382ee4f1cd
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-03-02 21:23:06 +0100

    feat: add password visibility toggle and improve input styling in AuthForm

--- frontend/src/components/auth/AuthForm.tsx
+++ frontend/src/components/auth/AuthForm.tsx
@@ -1,37 +1,90 @@
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { MdOutlinePerson, MdOutlineEmail } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
+import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 
 const AuthForm: React.FC = () => {
+  const [inputTypes, setInputTypes] = useState<{
+    password: string;
+    confirmPassword: string;
+  }>({
+    password: "password",
+    confirmPassword: "password",
+  });
   const pathName = usePathname();
+
+  const handlePasswordVisibility = (key: keyof typeof inputTypes) => {
+    setInputTypes((prevState) => ({
+      ...prevState,
+      [key]: prevState[key] === "password" ? "text" : "password",
+    }));
+  };
   return (
-    <form className="flex flex-col">
+    <form className="flex flex-col gap-5 justify-center items-center">
       {pathName === "/signUp" && (
-        <div>
-          <MdOutlinePerson />
-          <input type="text" name="name" placeholder="Name" />
-        </div>
-      )}
-      <div>
-        <MdOutlineEmail />
-        <input type="email" name="email" placeholder="Email" />
-      </div>
-      <div>
-        <RiLockPasswordLine />
-        <input type="password" name="password" placeholder="Password" />
-      </div>
-      {pathName === "/signUp" && (
-        <div>
-          <RiLockPasswordLine />
+        <div className="relative">
+          <MdOutlinePerson className="auth-input-icon" />
           <input
-            type="password"
-            name="confirm password"
-            placeholder="Confirm Password"
+            type="text"
+            name="name"
+            placeholder="Name"
+            className="auth-input"
           />
         </div>
       )}
-      <button type="submit" className="cursor-pointer">
+      <div className="relative">
+        <MdOutlineEmail className="auth-input-icon" />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          className="auth-input"
+        />
+      </div>
+      <div className="relative">
+        <RiLockPasswordLine className="auth-input-icon" />
+        <input
+          type={inputTypes.password}
+          name="password"
+          placeholder="Password"
+          className="auth-input"
+        />
+        {inputTypes.password === "password" ? (
+          <IoEyeOutline
+            className="absolute top-[50%] -translate-y-[50%] right-2.5 text-[20px]"
+            onClick={() => handlePasswordVisibility("password")}
+          />
+        ) : (
+          <IoEyeOffOutline
+            className="absolute top-[50%] -translate-y-[50%] right-2.5 text-[20px]"
+            onClick={() => handlePasswordVisibility("password")}
+          />
+        )}
+      </div>
+      {pathName === "/signUp" && (
+        <div className="relative">
+          <RiLockPasswordLine className="auth-input-icon" />
+          <input
+            type={inputTypes.confirmPassword}
+            name="confirm password"
+            placeholder="Confirm Password"
+            className="auth-input"
+          />
+          {inputTypes.confirmPassword === "password" ? (
+            <IoEyeOutline
+              className="absolute top-[50%] -translate-y-[50%] right-2.5 text-[20px]"
+              onClick={() => handlePasswordVisibility("confirmPassword")}
+            />
+          ) : (
+            <IoEyeOffOutline
+              className="absolute top-[50%] -translate-y-[50%] right-2.5 text-[20px]"
+              onClick={() => handlePasswordVisibility("confirmPassword")}
+            />
+          )}
+        </div>
+      )}
+      <button type="submit" className="green-btn">
         {pathName === "/signUp" ? "Sign Up" : "Sign In"}
       </button>
     </form>
