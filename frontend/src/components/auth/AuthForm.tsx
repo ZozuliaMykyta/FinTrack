c175b89675d2b8e25c25aadf078cb3cb0dbd631f
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-03-02 19:27:33 +0100

    update AuthForm

--- frontend/src/components/auth/AuthForm.tsx
+++ frontend/src/components/auth/AuthForm.tsx
@@ -1,21 +1,35 @@
 import { usePathname } from "next/navigation";
 import React from "react";
+import { MdOutlinePerson, MdOutlineEmail } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
 
 const AuthForm: React.FC = () => {
   const pathName = usePathname();
   return (
     <form className="flex flex-col">
       {pathName === "/signUp" && (
-        <input type="text" name="name" placeholder="Name" />
+        <div>
+          <MdOutlinePerson />
+          <input type="text" name="name" placeholder="Name" />
+        </div>
       )}
-      <input type="email" name="email" placeholder="Email" />
-      <input type="password" name="password" placeholder="Password" />
+      <div>
+        <MdOutlineEmail />
+        <input type="email" name="email" placeholder="Email" />
+      </div>
+      <div>
+        <RiLockPasswordLine />
+        <input type="password" name="password" placeholder="Password" />
+      </div>
       {pathName === "/signUp" && (
-        <input
-          type="password"
-          name="confirm password"
-          placeholder="Confirm Password"
-        />
+        <div>
+          <RiLockPasswordLine />
+          <input
+            type="password"
+            name="confirm password"
+            placeholder="Confirm Password"
+          />
+        </div>
       )}
       <button type="submit" className="cursor-pointer">
         {pathName === "/signUp" ? "Sign Up" : "Sign In"}
