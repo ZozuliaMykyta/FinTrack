3672f15c90ccc006e929b6ba9ac0484a3e33f079
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-03-05 22:02:39 +0100

    fix: update profile page layout with improved styling and structure

--- frontend/src/app/(main)/profile/page.tsx
+++ frontend/src/app/(main)/profile/page.tsx
@@ -3,9 +3,13 @@ import React from "react";
 
 const page: React.FC = () => {
   return (
-    <div className="container flex justify-between items-start">
-      <h3>My Profile</h3>
-      <UserInfo />
+    <div className="container mt-10">
+      <h3 className="text-dark-blue font-poppins text-[24px] leading-[0.48px] font-medium">
+        My Profile
+      </h3>
+      <div className="flex justify-between items-start mt-10">
+        <UserInfo />
+      </div>
     </div>
   );
 };
