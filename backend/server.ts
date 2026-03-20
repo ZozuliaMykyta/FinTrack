56a257ce4ef308a043b6ea4f500e3b464a1b7b7c
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-03-20 21:33:00 +0100

    feat: integrate AuthUserRoutes into server for user authentication

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import errorHandler from "./middlewares/errorMiddleware";
+import AuthUserRoutes from "./routes/AuthUserRoutes";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(errorHandler);
 //
 app.use(cors());
 app.use(express.json());
+app.use(AuthUserRoutes);
 
 // connect to MongoDB
 mongoose
