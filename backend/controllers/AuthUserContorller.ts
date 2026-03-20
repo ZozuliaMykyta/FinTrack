77f682a886fbf5ac6a69fe7b1ce9e210f9fdc947
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-03-20 21:06:14 +0100

    feat: update signUpController to handle user creation and improve error response

--- backend/controllers/AuthUserContorller.ts
+++ backend/controllers/AuthUserContorller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import bcrypt from "bcryptjs";
 import User from "../models/User";
 const signUpController = async (
   req: Request,
@@ -12,8 +13,16 @@ const signUpController = async (
     }
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: "Email already in use" });
+      return res.status(401).json({ message: "Email already in use" });
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name: username,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     next(error);
   }
