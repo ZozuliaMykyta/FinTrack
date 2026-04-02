1e92346587147278e1c689f2d02818e9162efa34
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-04-02 15:44:40 +0200

    feat: add email verification route to AuthUserRoutes

--- backend/routes/AuthUserRoutes.ts
+++ backend/routes/AuthUserRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import UserSignUpContorller from "../controllers/UserSignUpContorller";
+import emailVerifyController from "../controllers/emailVerifyController";
 
 const router = Router();
 
 router.post("/signup", UserSignUpContorller);
+router.get("/verify-email", emailVerifyController);
 
 export default router;
