7deb0df00b91ed42e6f010c38529b2a415af8313
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-04-23 19:45:27 +0200

    fix: update status code for existing user response from 401 to 409

--- backend/controllers/UserSignUpContorller.ts
+++ backend/controllers/UserSignUpContorller.ts
@@ -15,7 +15,7 @@ const signUpController = async (
     }
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(401).json({ message: "User already exists" });
+      return res.status(409).json({ message: "User already exists" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({
