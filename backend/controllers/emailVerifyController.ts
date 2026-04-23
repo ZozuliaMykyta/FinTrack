1ea16bbe4dc4f55c932e723a6748079f38b317ef
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-04-23 18:04:48 +0200

    feat: implement email verification controller

---
+++ backend/controllers/emailVerifyController.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+
+const emailVerifyController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { token } = req.query;
+    if (!token || typeof token !== "string") {
+      return res.status(400).json({ message: "Invalid token" });
+    }
+    const decoded = jwt.verify(
+      token,
+      process.env.EMAIL_SECRET || "your_secret",
+    ) as { email: string };
+    const user = await User.findOne({ email: decoded.email });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid credentials" });
+    }
+    if (user.isEmailVerified) {
+      return res.status(200).json({ message: "Email already verified" });
+    }
+    await user.updateOne({ isEmailVerified: true });
+
+    const sessionToken = jwt.sign(
+      { email: user.email },
+      process.env.JWT_SECRET || "your_jwt_secret",
+      {
+        expiresIn: "7d",
+      },
+    );
+
+    res
+      .status(200)
+      .json({ message: "Email verified successfully", token: sessionToken });
+  } catch (error) {
+    next(error);
+  }
+};
+export default emailVerifyController;
