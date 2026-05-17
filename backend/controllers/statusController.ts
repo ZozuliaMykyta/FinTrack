68803ebde75c5904103b6e19352eea7b483ffde0
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-05-17 17:10:53 +0200

    feat: add status controller to check email verification status of a user

---
+++ backend/controllers/statusController.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import User from "../models/User";
+const statusController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ message: "User ID is required" });
+    }
+
+    const user = await User.findOne({ id });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ isEmailVerified: user.isEmailVerified });
+  } catch (error) {
+    next(error);
+  }
+};
+export default statusController;
