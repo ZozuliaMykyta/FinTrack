8c513ae8e7b108da6ceb381e85785b8125e9b685
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-05-18 13:13:12 +0200

    fix: update user retrieval method to use findById in statusController

--- backend/controllers/statusController.ts
+++ backend/controllers/statusController.ts
@@ -12,7 +12,7 @@ const statusController = async (
       return res.status(400).json({ message: "User ID is required" });
     }
 
-    const user = await User.findOne({ id });
+    const user = await User.findById(id);
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
