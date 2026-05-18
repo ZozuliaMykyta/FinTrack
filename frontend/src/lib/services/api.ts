797ac2ecd9de0cbb9795a6c03b7a3f84be0f3822
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-05-18 13:12:21 +0200

    feat: implement user status endpoint and sign-up mutation in FinTrackApi

--- frontend/src/lib/services/api.ts
+++ frontend/src/lib/services/api.ts
@@ -1,13 +1,20 @@
-import IUser from "@/interfaces/IUser";
+import { ISignUpRequest, ISignUpResponse } from "@/interfaces/IAuthApi";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const FinTrackApi = createApi({
   reducerPath: "FinTrackApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
   endpoints: (builder) => ({
-    getUser: builder.query<IUser[], void>({
-      query: () => "/api/auth/users",
+    getUserStatus: builder.query<{ isEmailVerified: boolean }, string>({
+      query: (id) => `/api/auth/status/${id}`,
+    }),
+    signUpUser: builder.mutation<ISignUpResponse, ISignUpRequest>({
+      query: (credentials) => ({
+        url: "/api/auth/signup",
+        method: "POST",
+        body: credentials,
+      }),
     }),
   }),
 });
-export const { useGetUserQuery } = FinTrackApi;
+export const { useGetUserStatusQuery, useSignUpUserMutation } = FinTrackApi;
