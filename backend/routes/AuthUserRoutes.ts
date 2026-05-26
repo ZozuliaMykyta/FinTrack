71aa624ff79898ae53eaf862a2f3281c5328f085
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-05-26 15:34:59 +0200

    feat: add status route to AuthUserRoutes for user status retrieval

--- backend/routes/AuthUserRoutes.ts
+++ backend/routes/AuthUserRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import UserSignUpContorller from "../controllers/UserSignUpContorller";
 import emailVerifyController from "../controllers/emailVerifyController";
+import statusController from "../controllers/statusController";
 
 const router = Router();
 
 router.post("/signup", UserSignUpContorller);
 router.post("/verify-email", emailVerifyController);
+router.get("/status/:id", statusController);
 
 export default router;
