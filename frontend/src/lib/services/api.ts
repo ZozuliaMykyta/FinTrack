f6212209468043a704964ca003c987ccbb35a846
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-05-26 15:34:12 +0200

    feat: extend getUserStatus query to include userId and token in response

--- frontend/src/lib/services/api.ts
+++ frontend/src/lib/services/api.ts
@@ -5,7 +5,10 @@ export const FinTrackApi = createApi({
   reducerPath: "FinTrackApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
   endpoints: (builder) => ({
-    getUserStatus: builder.query<{ isEmailVerified: boolean }, string>({
+    getUserStatus: builder.query<
+      { isEmailVerified: boolean; userId: string; token: string },
+      string
+    >({
       query: (id) => `/api/auth/status/${id}`,
     }),
     signUpUser: builder.mutation<ISignUpResponse, ISignUpRequest>({
