98e80a904076885fb15f566b06ad2a48674907bd
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-05-26 15:34:43 +0200

    refactor: remove profile route and associated controller imports

--- backend/routes/profile.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import getProfile from "../controllers/profileController";
-import statusController from "../controllers/statusController";
-
-const router = express.Router();
-
-router.get("/status/:id", statusController);
-
-export default router;
