156ab6c83a7bdc177af03e813fdcfe7fea4c35f9
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-06-20 17:51:06 +0200

    added auth reducer to store

--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { FinTrackApi } from "./services/api";
+import authReducer from "@/lib/features/authSlice";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       [FinTrackApi.reducerPath]: FinTrackApi.reducer,
+      auth: authReducer,
     },
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(FinTrackApi.middleware),
