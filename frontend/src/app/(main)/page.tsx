539521b76496c88c0e9bdfe6807a886406218e6a
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-07-14 21:42:52 +0200

    implement user greeting in Home component(test)

--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -1,3 +1,10 @@
+"use client";
+import { useAppSelector } from "@/lib/hooks";
 export default function Home() {
-  return <></>;
+  const user = useAppSelector((state) => state.auth.user);
+  return (
+    <>
+      <h1>Welcome, {user?.name}!</h1>
+    </>
+  );
 }
