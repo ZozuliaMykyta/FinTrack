3ec39166814dfa42d4ad170e281e816c09109ee4
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-07-14 23:12:45 +0200

    feat: add token verification route to AuthUserRoutes

--- backend/routes/AuthUserRoutes.ts
+++ backend/routes/AuthUserRoutes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import UserSignUpContorller from "../controllers/UserSignUpContorller";
 import emailVerifyController from "../controllers/emailVerifyController";
 import statusController from "../controllers/statusController";
+import tokenVerifyController from "../controllers/tokenVerifyController";
 
 const router = Router();
 
 router.post("/signup", UserSignUpContorller);
 router.post("/verify-email", emailVerifyController);
 router.get("/status/:id", statusController);
+router.post("/token", tokenVerifyController);
 
 export default router;
