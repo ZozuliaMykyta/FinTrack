d3a98c554631299ed4c7475011dfb8fcf975698e
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-07-14 21:42:30 +0200

    add auth session handling in StoreProvider

--- frontend/src/app/StoreProvider.tsx
+++ frontend/src/app/StoreProvider.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { Provider } from "react-redux";
 import { makeStore } from "../lib/store";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
+import { setUserData } from "@/lib/features/authSlice";
 
 export default function StoreProvider({
   children,
@@ -9,5 +10,20 @@ export default function StoreProvider({
   children: React.ReactNode;
 }) {
   const store = useMemo(() => makeStore(), []);
+
+  useEffect(() => {
+    const authSession = localStorage.getItem("authSession");
+    if (!authSession) return;
+    try {
+      const sessionData = JSON.parse(authSession);
+      if (sessionData.user && sessionData.token) {
+        store.dispatch(
+          setUserData({ user: sessionData.user, token: sessionData.token }),
+        );
+      }
+    } catch {
+      localStorage.removeItem("authSession");
+    }
+  }, [store]);
   return <Provider store={store}>{children}</Provider>;
 }
