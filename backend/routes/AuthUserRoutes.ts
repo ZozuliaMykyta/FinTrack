11e9f10dc7db264379b7205115708c4c39922d45
ZozuliaMykyta <zozuliamykyta635@gmail.com>
2026-08-28 20:46:11 +0200

    feat: add route for refresh status with authentication middleware

--- backend/routes/AuthUserRoutes.ts
+++ backend/routes/AuthUserRoutes.ts
@@ -3,12 +3,15 @@ import UserSignUpContorller from "../controllers/UserSignUpContorller";
 import emailVerifyController from "../controllers/emailVerifyController";
 import statusController from "../controllers/statusController";
 import tokenVerifyController from "../controllers/tokenVerifyController";
+import refreshStatusController from "../controllers/refreshStatusController";
+import authMiddleware from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.post("/signup", UserSignUpContorller);
 router.post("/verify-email", emailVerifyController);
 router.get("/status/:id", statusController);
+router.get("/refresh-status", authMiddleware, refreshStatusController);
 router.post("/token", tokenVerifyController);
 
 export default router;
